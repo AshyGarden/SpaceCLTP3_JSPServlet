ffd82f67ef1732daa298872e1f67672c797f5882
youngungit <jrmrahn21@gmail.com>
2024-10-29 11:19:27 +0900

    login page by yh

--- mall/src/main/java/mall/servlet/LoginServlet.java
+++ mall/src/main/java/mall/servlet/LoginServlet.java
@@ -8,9 +8,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jy.servlet.JdbcSellerDao;
+import jy.servlet.SellerDao;
+import mall.younghun.Buyer;
+import mall.younghun.BuyerDao;
+import mall.younghun.JdbcBuyerDao;
 
 @SuppressWarnings("serial") 
-@WebServlet("/login") 
+@WebServlet("/login, /loginForm") 
 public class LoginServlet extends HttpServlet{
 	
 	@Override
@@ -24,19 +31,35 @@ public class LoginServlet extends HttpServlet{
 	}
 	
 	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.setCharacterEncoding("UTF-8");
-		
 		String uri = request.getRequestURI();
-		int lastIndex = uri.lastIndexOf("/"); 
-		String action = uri.substring(lastIndex+1); 
+		int lastIndex = uri.lastIndexOf("/");
+		String action = uri.substring(lastIndex + 1);
 		
-		System.out.println(action);
+		BuyerDao buyerDao = new JdbcBuyerDao();
+		SellerDao sellerDao = new JdbcSellerDao();
+		
+		if(action.equals("login")) {
+			String _id = request.getParameter("id");
+			String _password = request.getParameter("password");
+			String userType = request.getParameter("userType");
+			
+			HttpSession session = request.getSession();
+			if(userType.equals("buyer")) {
+				Buyer buyer = buyerDao.findById(_id);
+				
+				if(buyer != null && buyer.getPassword().equals(_password)) {
+					session.setAttribute("buyer", buyer);
+					
+				}else
+				
+			}else if(userType.equals("seller")) {
+				Seller seller;
+			}
 
-		String dispatcherUrl ="/pages/authentication/login.jsp";
+		}
 		
-		RequestDispatcher rd = request.getRequestDispatcher(dispatcherUrl);
-		rd.forward(request, response);
+		
+		
 		
 	}
 	
